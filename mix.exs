bbd12f9ff2389c02d4088a2199c4249b34e866ef
Krishnan Mani <km@krishnanm.com>
2021-09-01 07:50:35 +0100

    added coveralls for test coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule ElixirStarter.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
     ]
   end
 
@@ -23,6 +30,7 @@ defmodule ElixirStarter.MixProject do
     [
       {:mix_test_interactive, "~> 1.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 end
